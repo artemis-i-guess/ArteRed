2d5bd0ee430b3ebc670596b806a8e7bde6e3896a
Rishab Dugar <www.anitadugar9163@gmail.com>
2022-10-02 15:16:28 +0530

    Created .env, .gitignore and README files

---
+++ README.md
@@ -0,0 +1,9 @@
+# ArteRED
+
+## How to Install ? 
+
+1. Create a config.env file and add the following secrets into it
+   - USERNAME
+   - PASSWORD
+   - CLIENT_SECRET
+   - CLIENT_ID
--- reddit.py
+++ reddit.py
@@ -2,48 +2,56 @@ import discord
 import random
 import praw
 from discord.ext import commands
+import os
+from dotenv import load_dotenv # <- This is used to find and use .env files
 
+load_dotenv()
 
-reddit = praw.Reddit(client_id = "OEkJ7YUzngU4mutsyoMDGg",client_secret= "infroVmU5wjB2-Hefb1G3Hk11BAlrg", username = "Educational_Owl1957", password = "chessing63times", user_agent = "pythonwrapper")
+# Imported from config.env file -> 
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+USERNAME = os.getenv('USERNAME')
+PASSWORD = os.getenv('PASSWORD')
+
+reddit = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET,
+                     username=USERNAME, password=PASSWORD, user_agent="pythonwrapper")
 
 
 class emb(commands.Cog):
 
-  def __init__(self, client):
-    self.client = client
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def red(self, ctx, subred="memes"):
+
+        subreddit = reddit.subreddit(subred)
+        subsarray = []
+
+        for submission in subreddit.top(limit=50):
+            subsarray.append(submission)
+
+        randomsub = random.choice(subsarray)
+        name = randomsub.title
+        url = randomsub.url
+
+        embed = discord.Embed(title=name)
+        embed.set_image(url=url)
+
+        embed.set_footer(text="Requested by " + str(ctx.author.name))
+
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def gay(self, ctx, member: discord.Member):
+        embed = discord.Embed(title="Gaymeter", color=discord.Color.red())
+        value = random.randint(1, 100)
+
+        embed.add_field(name="The gaymeter for " + str(member.name) +
+                        " stands at " + str(value)+"%", value=member.name)
+
+        await ctx.send(embed=embed)
 
 
-  @commands.command()
-  async def red(self,ctx,subred= "memes"):
-    
-    subreddit = reddit.subreddit(subred)
-    subsarray=[]
-
-    for submission in subreddit.top(limit = 50):
-      subsarray.append(submission)
-
-    randomsub = random.choice(subsarray)
-    name = randomsub.title 
-    url = randomsub.url
-
-    embed = discord.Embed(title = name)
-    embed.set_image(url = url)
-
-    embed.set_footer(text = "Requested by " + str(ctx.author.name))
-    
-    await ctx.send(embed=embed)
-
-  @commands.command()
-  async def gay(self,ctx,member:discord.Member):
-    embed= discord.Embed(title = "Gaymeter",color = discord.Color.red())
-    value = random.randint(1,100)
-
-    embed.add_field(name = "The gaymeter for " + str(member.name) + " stands at " + str(value)+"%" ,value =member.name )
-    
-    await ctx.send(embed=embed)
-
-
-
-  
-def setup (client):
-  client.add_cog(emb(client))
+def setup(client):
+    client.add_cog(emb(client))
